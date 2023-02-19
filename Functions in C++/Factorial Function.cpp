903150e9744adc1363a1519a2f72e3068c9a59ad
Muhammad Adil Memon <96164867+muhammadadilmemon@users.noreply.github.com>
2023-02-19 18:11:20 +0500

    Create Factorial Function.cpp

---
+++ Functions in C++/Factorial Function.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int fact(int n){
+	int factorial=1;
+	for(int i=1;i<=n;i++){
+		factorial*=i;
+	}
+	return factorial;
+}
+int main(){
+	int n;
+	cin>>n;
+	int ans=fact(n);
+	cout<<ans<<endl;
+	
+	return 0;
+	
+}
