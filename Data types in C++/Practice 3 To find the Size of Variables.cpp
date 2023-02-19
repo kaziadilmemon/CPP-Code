aaf8d40efde3b5c46248ab3d00869678285259e2
Muhammad Adil Memon <96164867+muhammadadilmemon@users.noreply.github.com>
2023-02-19 17:51:28 +0500

    Create Practice 3 To find the Size of Variables.cpp

---
+++ Data types in C++/Practice 3 To find the Size of Variables.cpp
@@ -0,0 +1,8 @@
+int a=13;
+float d =14;
+long double e =16;
+cout<<"the size of 13 is "<<sizeof(13)<<endl;
+cout<<"the size of 14 is "<<sizeof(14)<<endl;
+cout<<"the size of 14F is "<<sizeof (14) <<endl;
+cout<<"the size of 16 is "<<sizeof (16)<<endl;
+cout<<"the size of 16L is "<<sizeof(16)<<endl; 
