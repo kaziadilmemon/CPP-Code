c4d2c03ba92c9fe4686d20b25f881f066d706c6a
Muhammad Adil Memon <96164867+muhammadadilmemon@users.noreply.github.com>
2023-02-22 09:23:08 +0500

    Create Inline Function.cpp

---
+++ Functions in C++/Inline Function.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+inline int product(int a,int b){
+	return a*b;
+}
+int main(){
+	int a,b;
+	cout<<"enter the value  of a and b"<<endl;
+	cin>>a>>b;
+	cout<<"the product of a and b is "<<product(a,b)<<endl;
+	cout<<"the product of a and b is "<<product(a,b)<<endl;
+	cout<<"the product of a and b is "<<product(a,b)<<endl;
+	cout<<"the product of a and b is "<<product(a,b)<<endl;
+	cout<<"the product of a and b is "<<product(a,b)<<endl;
+	cout<<"the product of a and b is "<<product(a,b)<<endl;
+	cout<<"the product of a and b is "<<product(a,b)<<endl;
+	cout<<"the product of a and b is "<<product(a,b)<<endl;
+	cout<<"the product of a and b is "<<product(a,b)<<endl;
+	return 0;
+}
