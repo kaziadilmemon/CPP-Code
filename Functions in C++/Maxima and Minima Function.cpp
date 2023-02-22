45c6101331f6b9eb1c4a96c9c175f2e4743dc3be
Muhammad Adil Memon <96164867+muhammadadilmemon@users.noreply.github.com>
2023-02-22 21:51:10 +0500

    Update Maxima and Minima Function.cpp

--- Functions in C++/Maxima and Minima Function.cpp
+++ Functions in C++/Maxima and Minima Function.cpp
@@ -1,1 +1,35 @@
+#include<iostream>
+using namespace std;
+int maxima(int i,int j,int k){
+	if(i>j&&i>k){
+		cout<<" first number is greatest"<<i<<endl;
+	}
+	if(j>k&&j>i){
+		cout<<"second number is greatest"<<j<<endl;
+	}
+	if(k>i&&k>j){
+		cout<<"third number is greatest"<<k<<endl;
+	}
+	return i,j,k;
+}
+int minima(int i,int j,int k){
+	if(i<j&&i<k){
+		cout<<" first number is smallest"<<i<<endl;
+	}
+	if(j<k&&j<i){
+		cout<<"second number is smallest"<<j<<endl;
+	}
+	if(k<i&&k<j){
+		cout<<"third number is smallest"<<k<<endl;
+	
+}
+return i,j,k;
+}
 
+int main(){
+	int i,j,k;
+	cin>>i>>j>>k;
+	maxima(i,j,k);
+	minima(i,j,k);
+	return 0;
+}
