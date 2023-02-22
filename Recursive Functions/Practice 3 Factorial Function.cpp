377fab47625e4493a2997f760affafff3ad874e0
Muhammad Adil Memon <96164867+muhammadadilmemon@users.noreply.github.com>
2023-02-22 22:09:46 +0500

    Update Practice 3 Factorial Function.cpp

--- Recursive Functions/Practice 3 Factorial Function.cpp
+++ Recursive Functions/Practice 3 Factorial Function.cpp
@@ -1,1 +1,14 @@
-
+#include<iostream>
+using namespace std;
+int factorial(int n){
+	if(n==0)
+	return 1;
+	int prevfactor=factorial(n-1);
+	return n*prevfactor;
+}
+int main(){
+	int n;
+	cin>>n;
+	cout<<factorial(n)<<endl;
+	return 0;
+}
