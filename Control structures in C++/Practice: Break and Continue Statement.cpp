2919a4015f2e73cfcc4a0e3cc422912fa35754f1
Muhammad Adil Memon <96164867+muhammadadilmemon@users.noreply.github.com>
2023-02-23 12:42:38 +0500

    Update Practice: Break and Continue Statement.cpp

--- Control structures in C++/Practice: Break and Continue Statement.cpp
+++ Control structures in C++/Practice: Break and Continue Statement.cpp
@@ -1,1 +1,14 @@
-
+#include<iostream>
+using namespace std;
+int main() {
+	int pocketmoney=3000;
+	for(int date=1;date<=30;date++){
+		if(date%2==0){
+			continue;
+		}
+		if (pocketmoney==0){
+			break;
+		}cout<<" go out today! "<<endl;
+	pocketmoney=pocketmoney-300;
+	} return 0;
+}
