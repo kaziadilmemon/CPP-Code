eba7173bb7fdec2270ca58a1ac00c395a82677af
Muhammad Adil Memon <96164867+muhammadadilmemon@users.noreply.github.com>
2023-02-23 14:20:19 +0500

    Update Practice 1.cpp

--- Arrays/Practice 1.cpp
+++ Arrays/Practice 1.cpp
@@ -1,1 +1,25 @@
+#include<iostream>
+using namespace std;
+int main() {
+int marks[]={13,15,39,98};
+cout<<marks[0]<<endl;
+cout<<marks[1]<<endl;
+cout<<marks[2]<<endl;
+cout<<marks[3]<<endl;
+//another example //
 
+int mathmarks[4];
+mathmarks[0]=2278;
+mathmarks[1]=738;
+mathmarks[2]=378;
+mathmarks[3]=1477;
+cout<<"these are math marks"<<endl;
+cout<<mathmarks[0]<<endl;
+cout<<mathmarks[1]<<endl;
+cout<<mathmarks[2]<<endl;
+cout<<mathmarks[3]<<endl;
+
+
+	return 0;
+	
+}
