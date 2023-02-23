ab3ddbe279bdc34a2360c869cccd45ae75f8b6ef
Muhammad Adil Memon <96164867+muhammadadilmemon@users.noreply.github.com>
2023-02-23 14:28:04 +0500

    Update Practice 1.cpp

--- Constructors/Practice 1.cpp
+++ Constructors/Practice 1.cpp
@@ -1,1 +1,25 @@
-
+#include<iostream>
+using namespace std;
+class employee{
+	public:
+		string name;
+		string company;
+		int age;
+	void  yourself(){
+		cout<<"Name -"<<name<<endl;
+		cout<<"Company -"<<company<<endl;
+		cout<<"Age -"<<age<<endl;
+		
+	}
+	employee(string name,string company,string int age){
+		Name=name;
+		Company=company;
+		Age=age;
+	}
+};
+int main(){
+	employee employee1=employee("saldina","YT-code beauty",25);
+	employee1.yourself();
+	employee employee2=employee("john","Amazon",35);
+	employee2.yourself();
+}
