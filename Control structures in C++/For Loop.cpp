970c110ba0d96e06b4bf8e2a8bbcdfa9a1ff036e
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-03-01 20:17:44 +0500

    Create For Loop.cpp

---
+++ Control structures in C++/For Loop.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a,b;
+	for(a=1;a<=10;a++){
+		cout<<a;
+		for(b=2;b<=2;b++){
+			if(a<=9){
+				cout<<"+";
+			}
+			else if(a=10){
+				cout<<"=";
+			}
+		}
+	}
+	cout<<"55";
+	return 0;
+}
