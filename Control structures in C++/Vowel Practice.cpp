e90187400a08e7d8bdb5688deddda22519599e72
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-03-01 20:48:52 +0500

    Update Vowel Practice.cpp

--- Control structures in C++/Vowel Practice.cpp
+++ Control structures in C++/Vowel Practice.cpp
@@ -1,1 +1,31 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main(){
+	char c;
+	cout<<"Enter any chacter\n";
+	cin>>c;
+	if (c=='a'||c=='e'||c=='i'||c=='o'||c=='u')
+	
+		cout<<"vowel";
+		
+	
+	else 
+	cout<<"consonent";
+	
+	getch();
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
