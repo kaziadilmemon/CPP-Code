d98fc47a186ee12bd57b49459d8c19db3e8ed4a4
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-03-01 20:31:13 +0500

    Update Practice 1.cpp

--- Structures in C++/Practice 1.cpp
+++ Structures in C++/Practice 1.cpp
@@ -1,1 +1,18 @@
+#include<iostream>
+using namespace std;
+struct store{
 
+int itemscarreid;
+float totalbill;
+float tax; 
+ };
+int main(){
+      	 store adil;
+	adil.itemscarreid=3;
+	adil.totalbill=3400;
+	adil.tax=3.4;
+	cout<<"the value of adil's items carried"<<adil.itemscarreid<<endl;
+	cout<<"the value of adil's total bill "<<adil.totalbill<<endl;
+	cout<<"the value of adil's tax "<<adil.tax<<endl;
+	return 0;
+}
