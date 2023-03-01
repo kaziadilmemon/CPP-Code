2b365bd725a4b38aa9815424625d088638f9ff70
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-03-01 20:35:54 +0500

    Update Practice 2: Union.cpp

--- Structures, Unions and Enumerations in C++/Practice 2: Union.cpp
+++ Structures, Unions and Enumerations in C++/Practice 2: Union.cpp
@@ -1,1 +1,13 @@
-
+#include<iostream>
+using namespace std;
+union balance{
+	float currentamount;
+	float totalamount;
+	
+};
+int main(){
+	balance b1;
+	b1.currentamount=34;
+	cout<<"current amount of b1  "<<b1.currentamount;
+	return 0;
+}
