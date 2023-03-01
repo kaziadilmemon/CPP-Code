0b9eda61c6184e2273f7724d72cfb97bcfb0abea
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-03-01 20:09:00 +0500

    Update Default Argument and Constant Argument.cpp

--- Default Argument and Constant Argument.cpp
+++ Default Argument and Constant Argument.cpp
@@ -1,1 +1,14 @@
+#include<iostream>
+using namespace std;
+float moneyreceived(int currentmoney,float factor=1.04){
+	return currentmoney*factor;
+}
+int main(){
+	int money=100000;
+	cout<<"if you have "<<money<<" Rs in your bank account,you will receive"<<moneyreceived(money)<<"Rs after 1 year"<<endl;
+	
+	cout<<"For vip: If you have"<<money<<"Rs in your bank account,you will receive"<<moneyreceived(money,1.4)<<" Rs after 1 year";
+	return 0;
+}
+	
 
