a9e226474911177eefd1a27398d66010c0c08efd
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-03-01 20:09:13 +0500

    Update Default Argument and Constant Argument.cpp

--- Default Argument and Constant Argument.cpp
+++ Default Argument and Constant Argument.cpp
@@ -8,7 +8,4 @@ int main(){
 	cout<<"if you have "<<money<<" Rs in your bank account,you will receive"<<moneyreceived(money)<<"Rs after 1 year"<<endl;
 	
 	cout<<"For vip: If you have"<<money<<"Rs in your bank account,you will receive"<<moneyreceived(money,1.4)<<" Rs after 1 year";
-	return 0;
-}
-	
-
+	return 0
