dcb00cbd94169f5defbe94438ff2e77b666ba05b
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-03-02 22:18:41 +0500

    Update Rectangle Pattern.cpp

--- Pattern Questions/Rectangle Pattern.cpp
+++ Pattern Questions/Rectangle Pattern.cpp
@@ -1,1 +1,14 @@
-
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main() {
+	int rows,col;
+	cin>>rows>>col;
+	for(int i=1;i<=rows;i++){
+		for(int j=1;j<=col;j++){
+			cout<<"8";
+		}
+		cout<<endl;
+	}
+	return 0;
+}
