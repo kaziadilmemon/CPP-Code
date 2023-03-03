7da531413efe6b25be8261ada2e499a3d7a1826f
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-03-03 10:00:20 +0500

    Update Hollow rectangle pattern.cpp

--- Pattern Questions/Hollow rectangle pattern.cpp
+++ Pattern Questions/Hollow rectangle pattern.cpp
@@ -1,1 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+int rows,col;
+cin>>rows>>col;
+for(int i=1;i<=rows;i++){
+	for(int j=1;j<=col;j++){
+		if(i==1||i==rows||j==1||j==col){
+			cout<<"*";}
+			else {
+				cout<<" ";
+			}
+		}
+		cout<<endl;
+	}
+	return 0;
+}
 
